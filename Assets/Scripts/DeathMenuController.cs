5aa9924a09a5af101986559782cac30cce4345a9
virejdasani <dasanivirej@gmail.com>
2020-09-05 12:27:04 +0530

    Added Start menu (MainMenu)

---
+++ Assets/Scripts/DeathMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathMenuController : MonoBehaviour
+{
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+    public void BackToLevelSelect()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenuButtonController.cs
@@ -0,0 +1,22 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButtonController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+}
