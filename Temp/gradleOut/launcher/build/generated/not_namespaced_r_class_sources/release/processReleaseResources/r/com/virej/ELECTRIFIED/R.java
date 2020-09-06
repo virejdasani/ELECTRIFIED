b40d9a64d46312fa000239a7392544cbcb1f4044
virejdasani <dasanivirej@gmail.com>
2020-09-06 12:43:52 +0530

    Added app logo

--- Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/virej/ELECTRIFIED/R.java
+++ Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/virej/ELECTRIFIED/R.java
@@ -14,8 +14,6 @@ public final class R {
   public static final class mipmap {
     public static final int app_icon=0x7f020000;
     public static final int app_icon_round=0x7f020001;
-    public static final int ic_launcher_background=0x7f020002;
-    public static final int ic_launcher_foreground=0x7f020003;
   }
   public static final class string {
     public static final int app_name=0x7f030000;
