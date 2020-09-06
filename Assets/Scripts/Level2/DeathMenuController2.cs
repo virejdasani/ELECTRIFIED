8b963e5691a4ddbe82cab36d0851f24466202da3
virejdasani <dasanivirej@gmail.com>
2020-09-06 11:49:43 +0530

    Added dead and win to Level2 and Level end Protection

---
+++ Assets/Scripts/Level2/DeathMenuController2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathMenuController2 : MonoBehaviour
+{
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+    public void BackToLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
---
+++ Assets/Scripts/Level2/LevelPassedButtonController2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelPassedButtonController2 : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
