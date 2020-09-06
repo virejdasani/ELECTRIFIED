dd110796a89815fa738f8ced8a9558449ad8da45
virejdasani <dasanivirej@gmail.com>
2020-09-06 09:55:27 +0530

    Added about scene

--- Assets/Scripts/PauseButtonController.cs
+++ Assets/Scripts/PauseButtonController.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PauseButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void levels()
     {
-        
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void about()
+    {
+        SceneManager.LoadScene("About");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
