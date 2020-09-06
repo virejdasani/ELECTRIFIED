2030bb0816746ce96d0c988ee665d664270b7326
virejdasani <dasanivirej@gmail.com>
2020-09-06 13:51:09 +0530

    Fixed a few bugs, final version in Builds>v1

---
+++ Assets/IngamePausebutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class IngamePausebutton : MonoBehaviour
+{
+    public void PauseMenu()
+    {
+        SceneManager.LoadScene("PauseMenu");
+    }
+    
+}
