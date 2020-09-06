4081fcadacf816722e60374ea609e92294078c47
virejdasani <dasanivirej@gmail.com>
2020-09-06 09:30:19 +0530

    Added a Level Selector

---
+++ Assets/Scripts/LevelSelectButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelectButtonController : MonoBehaviour
+{
+    public void level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void about()
+    {
+        SceneManager.LoadScene("About");
+    }
+}
